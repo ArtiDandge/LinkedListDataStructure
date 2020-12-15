e21a156eff7d8058e3870022c4592185d85c953c
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-15 15:27:26 +0530

    [Arti Dandge] Add : Create Simple linked list of 56, 30 and 70 and display it

--- LinkedListDataStructure/Program.cs
+++ LinkedListDataStructure/Program.cs
@@ -6,7 +6,14 @@ namespace LinkedListDataStructure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List Data Structure Problem");            
+            Console.WriteLine("Welcome to Linked List Data Structure Problem");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            Console.WriteLine("\nList Contains folowing data");
+            list.Display();
+            
         }
     }
 }
