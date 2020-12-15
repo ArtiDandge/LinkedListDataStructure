9c49899194d776abcfbfb71a62e6948d5aa8473a
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-15 15:16:54 +0530

    [Arti Dandge] Add : Display Welcome Message on Console

---
+++ LinkedListDataStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Data Structure Problem");            
+        }
+    }
+}
